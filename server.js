5e9b79d2a62ab3468fc1465c9439cf79b55fc1ab
youstina-attia <youstina.attia1@ucalgary.ca>
2022-04-07 00:13:08 -0600

    Setup server, db

---
+++ server.js
@@ -0,0 +1,28 @@
+// Server code reference: https://www.bezkoder.com/react-node-express-mysql/#Nodejs_Express_Back-end
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Savr application backend." });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
