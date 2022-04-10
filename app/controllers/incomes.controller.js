9790443a3a8dbefc3ff9e43c1572c8e177cb1e7d
youstina-attia <youstina.attia1@ucalgary.ca>
2022-04-09 22:41:45 -0600

    Finished incomes controller

--- app/controllers/incomes.controller.js
+++ app/controllers/incomes.controller.js
@@ -1,7 +1,7 @@
 // Controller reference: https://www.bezkoder.com/node-js-express-sequelize-mysql/#Create_the_Controller
 
 const db = require("../models");
-const incomes = db.incomes;
+const Incomes = db.incomes;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
@@ -23,7 +23,7 @@ exports.create = (req, res) => {
             date: entry.date,
             amount: entry.amount
           };
-          incomes.create(income)
+          Incomes.create(income)
           .then(data => {
             res.send(data);
           })
@@ -37,5 +37,36 @@ exports.create = (req, res) => {
 };
 
 exports.getMonthly = (req, res) => {
+        // Validate request
+        if (!req.param('months')||!req.param('userId')) {
+            res.status(400).send({
+                message: "Did not receive all required information: userID and months!"
+            });
+            return;
+        }
+        const userId = req.param('userID');
+        const months = req.param('months');
+        var monthsNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+        const results = []
+        for (let i = 0; i < months; i++) {
+            var firstDay = new Date(date.getFullYear(), date.getMonth()-i, 1);
+            var lastDay = new Date(date.getFullYear(), date.getMonth()+1-i , 0);
+            Incomes.sum('amount',{
+                where:{
+                    [Op.and]: {
+                    userID:userId,
+                    date: {
+                        [Op.gte]:firstDay,
+                        [Op.lte]:lastDay,
+                    }
+                    }
+                }
+            }).then(sum => {
+                results.push({
+                    month: monthsNames[date.getMonth()-i],
+                    amount: sum
+                })
+            })
+        }
+        res.send(results) 
 };
-
