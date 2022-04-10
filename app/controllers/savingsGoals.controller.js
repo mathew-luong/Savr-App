ac796d240fb72b783a7f3ebcc5807d76e356462a
youstina-attia <youstina.attia1@ucalgary.ca>
2022-04-09 18:35:48 -0600

    Added savings goals querying

--- app/controllers/savingsGoals.controller.js
+++ app/controllers/savingsGoals.controller.js
@@ -5,5 +5,28 @@ const SavingsGoals = db.savingsGoals;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
-};
+    // Validate request
+    if (!req.body.userID || !req.body.date || !req.body.amount) {
+        res.status(400).send({
+          message: "Did not receive all required savings goals information: userID, date and amount!"
+        });
+        return;
+      }
+      // Create savings goal
+      const savingsGoal = {
+        userId: req.body.userID,
+        date: req.body.date,
+        amount: req.body.amount
+      };
+      SavingsGoals.create(savingsGoal)
+            .then(data => {
+              res.send(data);
+            })
+            .catch(err => {
+              res.status(500).send({
+                message:
+                  err.message || "Some error occurred while creating the user."
+          });
+});
+}
 
