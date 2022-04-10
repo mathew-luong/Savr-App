37352c8069d0d3836eeee9b2bd2d40c5307d9784
youstina-attia <youstina.attia1@ucalgary.ca>
2022-04-09 22:08:45 -0600

    Added expense targets controller

--- app/controllers/expenseTargets.controller.js
+++ app/controllers/expenseTargets.controller.js
@@ -5,7 +5,57 @@ const expenseTargets = db.expenseTargets;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
+    // Validate request    
+    req.body.forEach(function(entry, index) {
+        if (!entry.userID || !entry.Category || !entry.percentageOfTotalIncome) {
+            res.status(400).send({
+                message: "Did not receive all required expenses information: userID, category and percentageOfTotalIncome!"
+            });
+            return;
+        }
+        
+    });
+    req.body.forEach(function(entry, index) { 
+    // Create expenses
+      const expenseTarget = {
+        userId: entry.userID,
+        category: entry.category,
+        percentage: entry.percentageOfTotalIncome
+      };
+      expenseTargets.create(expenseTarget)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the user."
+    });
+    });
+});
 };
 
 exports.getTargets = (req, res) => {
+    // Validate request
+    if (!req.param('userID')) {
+        res.status(400).send({
+            message: "Did not receive all required information: userID!"
+        });
+        return;
+    }
+    const userId = req.param('userID');
+    expenseTargets.findOne({
+        where: {
+          userId: userId
+        }
+      }).then(data => {
+          if(data){
+          res.send(data)
+        }else{
+            res.status(400).send({
+                message: "Did not find any expense targets!"
+            });
+            return;
+        }
+      })
 };
