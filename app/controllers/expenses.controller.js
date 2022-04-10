e43be1f8acec7767bae773ce2e053b5f997e7a09
youstina-attia <youstina.attia1@ucalgary.ca>
2022-04-10 11:53:05 -0600

    Finished expense controller querying

--- app/controllers/expenses.controller.js
+++ app/controllers/expenses.controller.js
@@ -138,10 +138,202 @@ exports.expensesInsightsChange = (req, res) => {
 };
 
 exports.expenseInsightsOverspent = (req, res) => {
+    // Validate request
+    if (!req.param('userID')) {
+        res.status(400).send({
+            message: "Did not receive all required information: userID!"
+        });
+        return;
+    }
+    const userId = req.param('userID');
+    const category = None;
+    const months = 0
+    //get expenses
+    var expenses;
+    const incomes = 0;
+    var targets;
+    const date = new Date();
+    const firstDay = new Date(date.getFullYear(), date.getMonth()-1, 1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() , 0);
+    Expenses.sum('amount',{
+        where:{
+            [Op.and]: {
+            userID:userId,
+            date: {
+                [Op.gte]:firstDay,
+                [Op.lte]:lastDay,
+            }
+            }
+        },
+        
+    },{group: 'category'}).then(data => {
+        expenses = data
+    })
+    //get income
+    Incomes.sum('amount',{
+        where:{
+            [Op.and]: {
+            userID:userId,
+            date: {
+                [Op.gte]:firstDay,
+                [Op.lte]:lastDay,
+            }
+            }
+        }
+    }).then(sum => {
+        incomes +=sum
+    })
+    //get expense target
+    expenseTargets.findAll({
+        where: {
+          userId: userId
+        }
+      }).then(data => {
+          if(data){
+            targets = data
+        }else{
+            res.status(400).send({
+                message: "Did not find any expense targets!"
+            });
+            return;
+        }
+      })
+      overspending = 0
+      expenses.forEach(function(expense,index){
+          expense.sum = expense.sum/incomes;
+          targets.forEach(function(target,index){
+            if (target.category == expense.category){
+                if(target.percentage<expense.sum){
+                    overspent = -1*(target.percentage - expense.percentage)/target.percentage
+                    if (overspending<overspent){
+                        underspending = underspent;
+                        category = target.category;
+                        months = 1
+                    }
+                }
+            }
+          })
+      })
+      const count = 0;
+      while(count<12){
+        const date = new Date();
+        const firstDay = new Date(date.getFullYear(), date.getMonth()-1-count, 1);
+        const lastDay = new Date(date.getFullYear(), date.getMonth() -count, 0);
+        Expenses.sum('amount',{
+            where:{
+                [Op.and]: {
+                userID:userId,
+                category:category,
+                date: {
+                    [Op.gte]:firstDay,
+                    [Op.lte]:lastDay,
+                }
+                }
+            },
+            
+        },{group: 'category'}).then(data => {
+            expenses = data
+        })
+        //get income
+        Incomes.sum('amount',{
+            where:{
+                [Op.and]: {
+                userID:userId,
+                date: {
+                    [Op.gte]:firstDay,
+                    [Op.lte]:lastDay,
+                }
+                }
+            }
+        }).then(sum => {
+            incomes =sum
+        })
+        overspending = 0
+        expenses.forEach(function(expense,index){
+            expense.sum = expense.sum/incomes;
+            targets.forEach(function(target,index){
+              if (target.category == expense.category){
+                  if(target.percentage<expense.sum){
+                      overspent = -1*(target.percentage - expense.percentage)/target.percentage
+                      if (overspending<overspent){
+                          underspending = underspent;
+                          category = target.category;
+                          months += 1;
+                      }else{
+                          count = 12;
+                      }
+                  }
+              }
+            })
+        })
+        count++;
+      }
+
+    res.send({
+        category: category,
+        numberOfMonths: months
+    })
 };
 
 exports.expensesBreakdown = (req, res) => {
+    // Validate request
+    if (!req.param('userID')) {
+        res.status(400).send({
+            message: "Did not receive all required information: userID!"
+        });
+        return;
+    }
+    const userId = req.param('userID');
+    const date = new Date();
+    const firstDay = new Date(date.getFullYear(), date.getMonth()-1, 1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() , 0);
+    Expenses.sum('amount',{
+        where:{
+            [Op.and]: {
+            userID:userId,
+            date: {
+                [Op.gte]:firstDay,
+                [Op.lte]:lastDay,
+            }
+            }
+        },
+        
+    },{group: 'category'}).then(data => {
+        res.send(data)
+    })
 };
 
 exports.totalExpenses = (req, res) => {
+    // Validate request
+    if (!req.param('months')||!req.param('userId')) {
+        res.status(400).send({
+            message: "Did not receive all required information: userID and months!"
+        });
+        return;
+    }
+    const userId = req.param('userID');
+    const months = req.param('months');
+    var monthsNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    const results = []
+    for (let i = 0; i < months; i++) {
+        var firstDay = new Date(date.getFullYear(), date.getMonth()-i, 1);
+        var lastDay = new Date(date.getFullYear(), date.getMonth()+1-i , 0);
+        Expenses.sum('amount',{
+            where:{
+                [Op.and]: {
+                userID:userId,
+                date: {
+                    [Op.gte]:firstDay,
+                    [Op.lte]:lastDay,
+                }
+                }
+            }
+        }).then(sum => {
+            results.push({
+                month: monthsNames[date.getMonth()-i],
+                amount: sum
+            })
+        })
+    }
+    res.send(results) 
 };
